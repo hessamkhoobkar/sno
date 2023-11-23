ea0d0b99ea72307af31a3f06846595f40ae2ae25
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-11-24 01:34:11 +0330

    Update font families in tailwind.config.js

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				serif: ["lora", ...defaultTheme.fontFamily.serif],
+				serif: ["Lora Variable", "Lora", ...defaultTheme.fontFamily.serif],
 				sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
 			},
 		},
