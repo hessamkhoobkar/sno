554922a5617e95ebe751ade2fa8e480e777512bc
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-12-06 21:24:45 +0330

    Add new layouts and update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Snø | Blog Theme";
+export const SITE_DESCRIPTION =
+  "Explore Snø, a free blog theme powered by Astro. Immerse your audience in a seamless blend of minimalism and sophistication. Elevate your online presence with lightning-fast performance, responsive design, and easy customization. Download Snø now for a delightful blogging experience.";
